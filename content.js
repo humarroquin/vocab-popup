80a72a3fefc0643e129e30dbb7e79001990d9879
Hugo <149089365+humarroquin@users.noreply.github.com>
2025-08-16 23:26:00 -0600

    added API call to show word, part of speech and pronunciation in UI

--- content.js
+++ content.js
@@ -1,6 +1,25 @@
 "use strict";
 
-document.addEventListener("mouseup", () => {
+async function getWordInfo(word) {
+  // fetch data from API
+  const res = await fetch(
+    `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+  );
+  // convert to JSON
+  const json = await res.json();
+
+  // select first entry
+  const entry = json[0];
+  const phonetics = entry.phonetics.find((p) => p.text);
+  const meaning = entry.meaning[0];
+
+  return {
+    partOfSpeech: meaning.partOfSpeech,
+    pronunciation: phonetics.text,
+  };
+}
+
+document.addEventListener("mouseup", async () => {
   // get the highlighted section
   const selection = window.getSelection().toString().trim();
   // if nothing gets selected do nothing
@@ -22,4 +41,16 @@ document.addEventListener("mouseup", () => {
   const range = window.getSelection().getRangeAt(0).getBoundingClientRect();
   popup.style.top = `${range.y + window.scrollY + 25}px`; // slightly below
   popup.style.left = `${range.x + window.scrollX}px`;
+
+  // API call
+  try {
+    const data = await getWordInfo(selection);
+    popup.innerHTML = `
+        <strong>${selection}</strong></br>
+        <em>${data.partOfSpeech}</em></br>
+        Pronunciation: ${data.pronunciation || "N/A"}
+    `;
+  } catch {
+    popup.innerText = "Could not fetch definition.";
+  }
 });
